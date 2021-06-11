59d5499c6a1c8f9afc26809ad0e2c9d469162a56
Drained1 <81043109+Drained1@users.noreply.github.com>
2021-06-11 01:52:51 -0500

    Update Automod.py

--- cogs/Automod.py
+++ cogs/Automod.py
@@ -15,10 +15,14 @@ passw = os.environ.get("pass")
 cluster = motor.motor_asyncio.AsyncIOMotorClient(f"mongodb+srv://{user}:{passw}@kacc.uozpt.mongodb.net/KACC?retryWrites=true&w=majority")
 database = cluster["Discord"]["Automod"]
 
+import dhooks
+from dhooks import Webhook
+
 def censor(text, bad_words):
     final = []
     text_split = text.split(" ")
     for word in text_split:
+        final_word = word
         if word.lower() in bad_words:
             final.append("\*"*len(word))
         else:
@@ -40,17 +44,29 @@ class Automod(commands.Cog):
         if message.author.bot:
             return
         
-        if message.author.guild_permissions.manage_messages:
-            return
+        #if message.author.guild_permissions.manage_messages:
+            #return
 
-        final = message.content.lower().split(" ")
+        first_text = message.content.lower().split(" ")
+        final = []
+
+        for word in first_text:
+            if "\u200b" in word:
+                final.append("".join([char for char in word if char != "\u200b"]))
+            else:
+                final.append(word)
+            print(word)
+            print(final)
 
         bad_words = info["bad_words"]
 
         for a in bad_words:
             if a in final:
                 await message.delete()
-                return await message.channel.send(embed=discord.Embed(description=censor(message.content, bad_words)).set_author(name=message.author.name, icon_url=message.author.avatar_url))
+                a = await message.channel.create_webhook(name = message.author.name)
+                await a.send(content = censor(message.content, bad_words), avatar_url = message.author.avatar_url_as(format='png', size=1024))
+                await a.delete()
+                return
 
     @commands.command()
     async def blacklist(self, ctx, word):
@@ -85,6 +101,19 @@ class Automod(commands.Cog):
 
         await database.update_one({"guild_id":ctx.guild.id}, {"$pull":{"bad_words":word}})
         await ctx.send(f'Successfully whitelisted {word}')
+
+    @commands.command()
+    async def blacklists(self, ctx):
+        if not ctx.author.guild_permissions.manage_messages:
+            return
+
+        info = await database.find_one({"guild_id":ctx.guild.id})
+        if not info:
+            await database.insert_one({"guild_id":ctx.guild.id, "bad_words":[None]})
+            return await ctx.send('I had to add your guild to my database. Please retry this command :)')
+
+        await ctx.send(embed=discord.Embed(title=f"Blacklists in {ctx.guild.name}", description="\n".join(info["bad_words"])))
+        
 
 def setup(client):
     client.add_cog(Automod(client))
